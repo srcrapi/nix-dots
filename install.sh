12f296f3fbf36c54483f4ffb50f80652ddabc7e8
srcrap1 <rafa1000.skylanders@gmail.com>
2024-06-12 20:59:23 +0100

    fix: Add missing themes

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env sh
 
 saveDir="$HOME/nix"
+confPath="$HOME/.config"
 scrDir=$(dirname "$(realpath "$0")")
 
-cp -r "${srcDirs}/swaylock/" "$HOME/.config"
+cp -r "${srcDirs}/swaylock/" "$confPath"
+cp -r "${srcDirs}/rofi/" "$confPath"
+
+if [ ! -f "${confPath}/waybar" ]; then
+  mkdir -p "${confPath}/waybar"
+fi
+
+cp -r "${srcDirs}/theme.css" "$confPath/waybar"
 
 mkdir -p "$saveDir"
 
